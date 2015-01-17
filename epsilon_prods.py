482399e9d43a52691d45b0c877deef2cae04797e
MasterChief93 <fede93.vagnoni@gmail.com>
2015-01-17 10:04:52 +0100

    - Added comments to unit_prods.py - Added comments to epsilon_prods.py - Some correction

--- epsilon_prods.py
+++ epsilon_prods.py
@@ -2,35 +2,35 @@ __author__ = 'MasterChief93'
 from useless_symbs import useless_symbs
 
 def e_prod_elim(term,nonterm,prod):
-    null = [0]*len(nonterm)
+    null = [0]*len(nonterm) # sets of nullable symbols
     for nterm in nonterm:
         count = 0
-        for pro in prod:
+        for pro in prod:     # We count how many production that non-terminal has
             if pro[0] == nterm:
                 count += 1
-        if count == 1 and [nterm,"e"] in prod:
-            null[nonterm.index(nterm)] = 1
-    for pro in prod:
+        if count == 1 and [nterm,"e"] in prod: # if it has only one production and that production is epsilon
+            null[nonterm.index(nterm)] = 1     # it is a nullable symbol
+    for pro in prod:           # for the symbols produced
         for symb in pro[1]:
-            if symb in nonterm:
-                if null[nonterm.index(symb)] == 1:
-                    pro[1] = pro[1].replace(symb,"")
+            if symb in nonterm:   #if that symbols are non-terminals
+                if null[nonterm.index(symb)] == 1:  #if that symbols are nullable
+                    pro[1] = pro[1].replace(symb,"")  # we can replace their presence with an epsilon-string (empty)
     for pro in prod:
-        if pro[1] == "":
-            null[nonterm.index(pro[0])] = 1
-    if null[nonterm.index("S")] == 1 and ["S","e"] not in prod:
+        if pro[1] == "":   # if a production is an empty (all the symbols produced were nullable)
+            null[nonterm.index(pro[0])] = 1  # that symbol is nullable too
+    if null[nonterm.index("S")] == 1 and ["S","e"] not in prod:   # if S is nullable, we add ['S','e'] to productions
         prod.append(["S","e"])
     newprod = []
     for pro in prod:
-        if not((pro[1] == "e" or pro[1] == "") and pro != ["S","e"]):
-            newprod.append(pro)
-    tempprod = []
-    tempprod.extend(newprod)
-    for pro in tempprod:
-        if pro[0] not in term + nonterm:
-            newprod.remove(pro)
-            continue
-        for symb in pro[1]:
-            if symb not in term + nonterm + ["e"]:
-                newprod.remove(pro)
+        if not((pro[1] == "e" or pro[1] == "") and pro != ["S","e"]): # if the symbol is not nullable and the production
+            newprod.append(pro)                                        # is differente from ['S','e'] than we can add it
+    # tempprod = []                                                       # to the new set of productions
+    # tempprod.extend(newprod)
+    # for pro in tempprod:
+    #     if pro[0] not in term + nonterm:
+    #         newprod.remove(pro)
+    #         continue
+    #     for symb in pro[1]:
+    #         if symb not in term + nonterm + ["e"]:
+    #             newprod.remove(pro)
     return useless_symbs(term,nonterm,newprod)
--- unit_prods.py
+++ unit_prods.py
@@ -10,36 +10,44 @@ def trivial(prod):
     return newprod
 
 def unitprod(term,nonterm,prod):
-    result = e_prod_elim(term,nonterm,prod)
+    result = e_prod_elim(term,nonterm,prod) # this algorithm work only with grammar without epsilon-productions
     term = result[0]
     nonterm = result[1]
     prod = result[2]
+    # We create the queue where we put (in any order) all the non-trivial unit-productions in the grammar
     queue = []
     while True:
-        prod = trivial(prod)
-
+        prod = trivial(prod) # elimination of all trivial productions
         for pro in prod:
-            if pro[0] != pro[1] and len(pro[0]) == len(pro[1]) == 1 and pro[0] in nonterm and pro[1] in nonterm:
+            if len(pro[0]) == len(pro[1]) == 1 and pro[0] in nonterm and pro[1] in nonterm:
                 if pro not in queue:
-                    queue.append(pro)
+                    queue.append(pro) # we can add to the queue the new unit-production
+        if len(queue) == 0:
+            print useless_symbs(term,nonterm,prod)
+            return
+        # Then we pop the first one
         selected_prod = queue[0]
+        # and reduce the queue
         queue = queue[1:]
         tempprod = []
         tempprod.extend(prod)
         for pro in tempprod:
             if pro == selected_prod:
-                for pro2 in tempprod:
+                for pro2 in tempprod:  # Replacement of the symbol produced in the unit-production with its productions
                     if pro[1] == pro2[0] and [pro[0],pro2[1]] not in prod:
                         prod.append([pro[0],pro2[1]])
-                prod.remove(selected_prod)
+                prod.remove(selected_prod) # We can now remove that unit-production from productions
+        # Elimination of every trivial unit-production occurred
         prod = trivial(prod)
+        # The cicle end when the queue is empty
         if queue == []:
             break
+    # Elimination of every useless-symbols occured and return
     print useless_symbs(term,nonterm,prod)
 
 
-term = ["a","b"]
-nonterm = ["S","A","B"]
-prod = [["S","AS"],["S","A"],["A","a"],["A","B"],["B","b"],["B","S"],["B","A"]]
+term = ["+","x",'(',')','a']
+nonterm = ["S","T","F"]
+prod = [["S","S+T"],["S","T"],["T","TxF"],["T","F"],["F","(S)"],["F","a"]]
 
 unitprod(term,nonterm,prod)
